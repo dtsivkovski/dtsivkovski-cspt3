21044fd1c247fb904e4e21c4972b9e92646097eb
Daniel Tsivkovski <89223402+dtsivkovski@users.noreply.github.com>
2022-03-16 21:14:27 +0000

    added loops and math functions

---
+++ python/math.py
@@ -0,0 +1,20 @@
+# Factorial of a number using recursion
+def recur_factorial(n):
+    if n == 1 or n == 0:
+        return 1
+    else:
+        return n * recur_factorial(n-1)
+
+def fibonacci():
+  print("Function not working at the moment")
+
+# this is test driver or code that plays when executed directly, versus import which will not run these statements
+def tester():
+    num = int(input("Enter a number for factorial: "))
+    # check if the number is negative
+    if num < 0:
+        print("Sorry, factorial does not exist for negative numbers")
+    else:
+        print("The factorial of", num, "is", recur_factorial(num))
+
+# Hack 3: Fibonacci.  Write a recursive program to create a fibonacci sequence including error handling(with try/except) for invalid input
